f55558e2c21cc6ddc33d108680b1178f627b8719
Winicius Alexandre <winiciusalexandre@gmail.com>
2024-05-15 18:35:40 -0300

    Adicionando retorno do terminal no Main do Abstract Factory

--- Abstract Factory/src/main/Main.java
+++ Abstract Factory/src/main/Main.java
@@ -11,7 +11,40 @@ public class Main {
 
 	public static void main(String[] args) {
 		usandoFabricaPagamentosPaypal();
+
+		// RETORNO NO TERMINAL
+
+		// Enviando transacao para paypal.com
+		// transacao: {cartao: 7204835286027102150cliente.senha: [B@254989ffcliente.cpf: 123.456.993-22 paypal paypalestabelecimento: 1001 paypalcompra.valor: 2000.0compra.parcelas 10compra.juros0.02compra.data1715808661561modo: new}
+		// chave SHA-256
+		// [B@5ef04b5
+
+
 		usandoFabricaPagamentosPagseguro();
+
+		// RETORNO NO TERMINAL
+
+		// Enviando transacao para pagseguro.com
+		// <transacao>
+		//   <cartao>
+		// 	    <numero>7204071238745102</numero>
+		// 	    <cvv>220</cvv>
+		//   </cartao>
+		//   <cliente>
+		// 	    <senha>[B@4459eb14</senha>
+		// 	    <cpf>321.654.674-01 pagseguro pagseguro</cpf>
+		//   </cliente>
+		//   <estabelecimento>1001 pagseguro</estabelecimento>
+		//   <compra>
+		// 	    <valor>5000.0</valor>
+		// 	    <parcelas>10</parcelas>
+		// 	    <juros>0.04</juros>
+		// 	    <data>1715808661629</data>
+		//   </compra>
+		//   <modo>new</modo>
+		// </transacao>
+		// chave MD5
+		// [B@6659c656
 	}
 
 	public static void usandoFabricaPagamentosPaypal() {
